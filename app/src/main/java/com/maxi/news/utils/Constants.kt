62503bda313635c91e28b51f663cb3878e5a978f
maxi <arora.anubhav93@gmail.com>
2025-05-23 17:31:14 +0530

    Add options constants

--- app/src/main/java/com/maxi/news/utils/Constants.kt
+++ app/src/main/java/com/maxi/news/utils/Constants.kt
@@ -5,4 +5,9 @@ object Constants {
     const val DEFAULT_COUNTRY = "us"
     const val DEFAULT_LANGUAGE = "en"
     const val USER_AGENT = "maxi-android"
+    const val TOP_HEADLINES = "Top Headlines"
+    const val NEWS_SOURCES = "News Sources"
+    const val COUNTRIES = "Countries"
+    const val LANGUAGES = "Languages"
+    const val SEARCH = "Search"
 }
