366e4384be00f1934ef33ed325d33f3c058276cf
maxi <arora.anubhav93@gmail.com>
2025-05-23 16:47:32 +0530

    Add api call methods

---
+++ app/src/main/java/com/maxi/news/data/remote/NetworkService.kt
@@ -0,0 +1,18 @@
+package com.maxi.news.data.remote
+
+import com.maxi.news.domain.model.HeadlineResponse
+import com.maxi.news.domain.model.NewsSourcesResponse
+import com.maxi.news.utils.Constants.DEFAULT_LANGUAGE
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NetworkService {
+
+    @GET("top-headlines")
+    suspend fun getHeadlines(
+        @Query("language") language: String = DEFAULT_LANGUAGE
+    ): HeadlineResponse
+
+    @GET("top-headlines/sources")
+    suspend fun getSources(): NewsSourcesResponse
+}
